d986d3e057ec7306150b5807599b3c9239ec192d
ChenWeizhao <1335847336@qq.com>
2019-04-21 19:46:27 +0800

    完成登录UI，文件分类

---
+++ XianYu/app/src/main/java/com/example/chenweizhao/xianyu/Activities/FindPasswordActivity1.java
@@ -0,0 +1,14 @@
+package com.example.chenweizhao.xianyu.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.chenweizhao.xianyu.R;
+
+public class FindPasswordActivity1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_password_by_message);
+    }
+}
---
+++ XianYu/app/src/main/java/com/example/chenweizhao/xianyu/Activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.chenweizhao.xianyu.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.chenweizhao.xianyu.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
