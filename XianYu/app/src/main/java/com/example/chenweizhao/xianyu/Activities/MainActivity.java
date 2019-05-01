fa0d69811bbe09659d26f51893b3240c0a36d44a
Xu Jj <1443239772@qq.com>
2019-05-01 20:17:02 +0800

    Xu finish UI

---
+++ XianYu/app/src/main/java/com/example/chenweizhao/xianyu/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.chenweizhao.xianyu.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.chenweizhao.xianyu.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
