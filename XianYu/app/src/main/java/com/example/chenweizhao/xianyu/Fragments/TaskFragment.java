ef73952568caf87181d326c9d8be0e5ee1728063
ChenWeizhao <1335847336@qq.com>
2019-05-04 20:39:25 +0800

    finish weizhao's UI

---
+++ XianYu/app/src/main/java/com/example/chenweizhao/xianyu/Fragments/TaskFragment.java
@@ -0,0 +1,22 @@
+package com.example.chenweizhao.xianyu.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.chenweizhao.xianyu.R;
+
+public class TaskFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+        return view;
+    }
+}
